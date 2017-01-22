7edf3c7279141dc6026acaa3015316ea7146b8d3
vitorsvieira <vitorsv.vieira@gmail.com>
2017-01-22 19:26:38 +0100

    Fresh project, created with sbt-fresh

---
+++ src/main/scala/com/vitorsvieira/scala/iso/package.scala
@@ -0,0 +1,10 @@
+
+package com.vitorsvieira.scala
+
+package object iso {
+
+  type Traversable[+A] = scala.collection.immutable.Traversable[A]
+  type Iterable[+A]    = scala.collection.immutable.Iterable[A]
+  type Seq[+A]         = scala.collection.immutable.Seq[A]
+  type IndexedSeq[+A]  = scala.collection.immutable.IndexedSeq[A]
+}
